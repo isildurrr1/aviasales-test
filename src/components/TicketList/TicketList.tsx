1188263f6391467ff94f0f3637fb5f79a49114a7
Andrey Romashev <andreygaranin.0@yandex.ru>
2024-10-26 14:18:22 +0700

    add placeholder antd

--- src/components/TicketList/TicketList.tsx
+++ src/components/TicketList/TicketList.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { Empty } from 'antd'
 
 import { useAppSelector } from '../../hooks/hooks'
 import Tabs from '../Tabs/Tabs'
@@ -10,6 +11,7 @@ import { filterTickets } from '../../utils/func'
 const TicketList = () => {
   const tickets = useAppSelector((state) => state.tickets.list)
   const checkboxesState = useAppSelector((state) => state.checkboxes)
+  const checkboxSelected = Object.values(checkboxesState.transfers).includes(true)
   const [ticketsPerPage, setTicketsPerPage] = useState(5)
   return (
     <div className="ticket-list">
@@ -20,12 +22,12 @@ const TicketList = () => {
         .map((ticket) => (
           <Ticket ticket={ticket} key={uuidv4()} />
         ))}
-      {tickets.length !== 0 && Object.values(checkboxesState.transfers).includes(true) ? (
+      {tickets.length !== 0 && checkboxSelected ? (
         <button type="button" className="ticket-list__button" onClick={() => setTicketsPerPage(ticketsPerPage + 5)}>
           Показать еще 5 билетов!
         </button>
       ) : (
-        <h4>Рейсов, подходящих под заданные фильтры, не найдено</h4>
+        <Empty description={!checkboxSelected && <span>Рейсов, подходящих под заданные фильтры, не найдено</span>} />
       )}
     </div>
   )
